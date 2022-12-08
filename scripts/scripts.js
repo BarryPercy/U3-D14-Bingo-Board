4f24eb74bf9473694cd3178320120c9a7ebaba74
Barry Percy <baz.percy@gmail.com>
2022-12-08 13:40:00 +0000

    BIngo Board/number generator created

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,27 @@
-window.onload = alert("hey");
+window.onload = function(){
+    createBingoBoard()
+}
+let alreadySelectedNumbers= [];
+let bingoBoard = document.getElementById("bingo-board")
+function createBingoBoard(){
+    for(let i=1;i<=76;i++){
+        newNumberDiv= document.createElement('div');
+        newNumberDiv.innerHTML = "<p>"+[i]+'</p>';
+        bingoBoard.appendChild(newNumberDiv);
+    }
+}
+function generateNumber(){
+    let number = Math.floor(Math.random()*76)+1;
+    if(alreadySelectedNumbers.includes(number)){
+        generateNumber();
+    }else{}
+        alreadySelectedNumbers.push(number);
+        highlightBingoNumber(number);
+        console.log(number);
+}
+
+function highlightBingoNumber(num){
+    let numberDiv = document.querySelectorAll("#bingo-board div")[num-1];
+    console.log(numberDiv);
+    numberDiv.classList.add("selectedNumber");
+}
